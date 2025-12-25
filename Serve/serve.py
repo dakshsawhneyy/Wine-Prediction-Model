cf8f93d37f7af80fe6aefebce4a40913f0ed2139
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-25 18:50:01 +0530

    /metrics endpoint exposed to serving

--- Serve/serve.py
+++ Serve/serve.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
 
+from prometheus_client import Counter, Histogram, Gauge, generate_latest, CONTENT_TYPE_LATEST
+import time
+
 load_dotenv()
 
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
@@ -21,6 +24,11 @@ model = mlflow.pyfunc.load_model(
     model_uri=f"models:/{MODEL_NAME}/{MODEL_STAGE}"
 )
 
+
+# Prometheus Metrics
+PREDICTION_COUNT = Counter("prediction_requests_total", "Total predictions made")
+PREDICTION_LATENCY = Histogram("prediction_latency_seconds", "Latency for predictions")
+
 app = FastAPI()
 
 class WineFeatures(BaseModel):
@@ -31,7 +39,13 @@ def health():
     return{"status": "okay"}
 
 @app.post('/predict')
+@PREDICTION_LATENCY.time()
 def predict(data: WineFeatures):
     df = pd.DataFrame([data.features])
     preds = model.predict(df)
+    PREDICTION_COUNT.inc()
     return {"prediction": float(preds[0])}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
