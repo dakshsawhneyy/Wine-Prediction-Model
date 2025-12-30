9910817e91ed352cd64912d65deb0746047e65a6
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-30 21:51:19 +0530

    Fixed LB Name

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,4 +1,4 @@
 output "load_balancer_ip" {
   description = "The public URL for the application load balancer."
-  value = "https://${aws_lb.my_lb.dns_name}"
+  value = "https://${aws_lb.alb.dns_name}"
 }
